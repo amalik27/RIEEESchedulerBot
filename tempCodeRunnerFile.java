dc881e2a2fb1a6d9a16b13419a0ca55c094024af
Vraj Panchal <vrajpanchal3@gmail.com>
2022-03-26 17:32:49 -0400

    My commit

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,8 @@
+import java.util.Scanner
+public class MyClass{
+    public static void main(String args[]){
+    Scanner fauzanScanner = new Scanner(System.in);
+    String name = fauzanScanner.nextLine();
+    System.out.println("Hello, " + name);
+}
+}
