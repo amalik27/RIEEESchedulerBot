81690ef6c7cceb433396a4f7087656736c652bf6
Christinayoo14 <102433494+Christinayoo14@users.noreply.github.com>
2022-03-26 18:04:49 -0400

    Delete tempCodeRunnerFile.java
    lol

--- tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner
-public class MyClass{
-    public static void main(String args[]){
-    Scanner fauzanScanner = new Scanner(System.in);
-    String name = fauzanScanner.nextLine();
-    System.out.println("Hello, " + name);
-}
-}
