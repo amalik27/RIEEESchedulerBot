16dc32e782770f002dfa2856da84302f8fb07d84
Christinayoo14 <102433494+Christinayoo14@users.noreply.github.com>
2022-03-27 01:01:35 -0400

    messager

--- main.py
+++ main.py
@@ -14,41 +14,28 @@
 # client.run(token)
 
 import discord
-from discord.ext import commands
-from discord.ext import tasks
-import asyncio
-import datetime
-import time
+# from discord.ext import commands
+# from discord.ext import tasks
+# import asyncio
+# import datetime
+# import time
+import os
 
 TOKEN = 'OTU3MzUyNDQzNjYwODgyMDIw.Yj9h8Q.detpUGrH4oU0Gcp28ycS3mVQ4gQ' # discord bot token
 
-client = commands.Bot(command_prefix = '.')
+client = discord.Client()
 
-channel_id = '957476426632798208' # set it to proper channel ID
+# channel_id = '957476426632798208' # set it to proper channel ID
 
 @client.event
 async def on_ready():
-    print('Scheduler Bot: Up and ready for action!')
+    print('{0.user}]: Up and ready for action!'.format.client)
 
-# @tasks.loop(days=1)
-async def alarm_message():
-    await client.wait_until_ready()
-    while not client.is_closed:
-        channel = client.get_channel(channel_id)
-        messages = ('test')
-        await client.send_message(channel, messages)
-        await asyncio.sleep(5) #runs every 5 seconds
-
-client.loop.create_task(alarm_message())
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+	elif message.content.startswith('!schedule'):
+		await message.channel.send('You got it!')
 
 client.run(TOKEN)
-
-# @client.event
-# async def on_message(message):
-
-# 	if message.author == client.user:
-# 		return
-# 		elif message.content.startswith('!setMessage'):
-# 			await message.channel.send('I am alive!')
-	
-# 		client.run(TOKEN)
