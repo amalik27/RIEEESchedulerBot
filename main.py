17834b987db902936cdf76bf10691934d408bfc8
Christinayoo14 <102433494+Christinayoo14@users.noreply.github.com>
2022-03-27 00:34:37 -0400

    timed message draft

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
-import discord
-token = "OTU3Mzg5MDY3OTI0OTM0NjY2.Yj-EDQ.kV3VcBA66rQPKqQXNwjS-MCrFio"
+# import discord
+# token = "OTU3Mzg5MDY3OTI0OTM0NjY2.Yj-EDQ.kV3VcBA66rQPKqQXNwjS-MCrFio"
 
-client = discord.Client()
+# client = discord.Client()
+# @client.event
+# async def on_ready():
+# 	print("{0.user} is online!".format.client)
+# @client.event
+# async def on_message(message):
+# 	if message.author == client.user:
+# 		return
+# 	elif message.content.startswith('!setMessage'):
+# 		await message.channel.send('I am alive!')
+# client.run(token)
+
+import discord
+from discord.ext import commands
+from discord.ext import tasks
+import asyncio
+import datetime
+import time
+
+TOKEN = 'OTU3MzUyNDQzNjYwODgyMDIw.Yj9h8Q.detpUGrH4oU0Gcp28ycS3mVQ4gQ' # discord bot token
+
+client = commands.Bot(command_prefix = '.')
+
+channel_id = '957476426632798208' # set it to proper channel ID
+
 @client.event
 async def on_ready():
-	print("{0.user} is online!".format.client)
+    print('Scheduler Bot: Up and ready for action!')
+
+@tasks.loop(days=1)
+async def alarm_message():
+    await client.wait_until_ready()
+    channel = client.get_channel(channel_id)
+    message = 'test'
+    await channel.send(message)
+    
+    alarm_message.start()
+
 @client.event
 async def on_message(message):
-	if message.author == client.user:
-		return
-	elif message.content.startswith('!setMessage'):
-		await message.channel.send('Im alive!')
-client.run(token)
+
+    time = datetime.datetime.today()
+
+    if message.author == client.user:
+        return
+    elif message.content.startswith('!schedule {time.hour}:{time.minute}'):
+        await client.get_channel(channel_id).send(f"It is {time.hour}:{time.minute}!")
+
+    client.run(TOKEN)
