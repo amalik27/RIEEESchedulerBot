77130cea540346c7723cf497a079d7540becad4c
Christinayoo14 <102433494+Christinayoo14@users.noreply.github.com>
2022-03-27 02:05:57 -0400

    man wtf

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ import discord
 # from discord.ext import commands
 # from discord.ext import tasks
 # import asyncio
-import datetime
-import time
+# import datetime
+# import time
 # import os
 
 TOKEN = 'OTU3Mzg5MDY3OTI0OTM0NjY2.Yj-EDQ.goc40Ay9l-LhzgOzfTDxQ5pOSg8' # discord bot token
 
 client = discord.Client()
 
-channel_id = '957476426632798208' # set it to proper channel ID
+# channel_id = '957476426632798208' # set it to proper channel ID
 
 @client.event
 async def on_ready():
@@ -36,12 +36,12 @@ async def on_message(message):
 
 	if message.author == client.user:
 		return
-	#if message.content.startswith('!schedule'):
-	#	await message.channel.send('You got it!')
-	while True:
-		time = datetime.datetime.today()
-		
-		if message.content.startswith('!schedule {time.hour}:{time.minute}'):
-			await client.get_channel(channel_id).send(f"It is {time.hour}:{time.minute}!")
+	if message.content.startswith('!schedule'):
+		await message.channel.send('You got it!')
+	# while True:
+	# 	time = datetime.datetime.today()
+
+	# 	if message.content.startswith('!schedule {time.hour}:{time.minute}'):
+	# 		await client.get_channel(channel_id).send(f"It is {time.hour}:{time.minute}!")
 
 client.run(TOKEN)
