42c70aa3db616c676ac3eb5577f12ab2b79056ed
Christinayoo14 <102433494+Christinayoo14@users.noreply.github.com>
2022-03-27 03:41:35 -0400

    Up and ready for action

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
 import discord
-# from discord.ext import commands
-from discord.ext import tasks
 import asyncio
-import datetime
+from discord.ext import commands
+from discord.ext import tasks
 import time
 # import os
+# import datetime
 
-TOKEN = 'OTU3Mzg5MDY3OTI0OTM0NjY2.Yj-EDQ.goc40Ay9l-LhzgOzfTDxQ5pOSg8'  # discord bot token
+TOKEN = 'OTU3Mzg5MDY3OTI0OTM0NjY2.Yj-EDQ.3Ld43lvx2v3qk1DVYSxrDyHwTks'  # discord bot token
 
 client = discord.Client()
 
 channel_id = '957476426632798208'  # set it to proper channel ID
 
+
 @client.event
 async def on_ready():
-    print('{0.user}: Up and ready for action!'.format(client))
+    print('Up and ready for action!')
 
-@tasks.loop(days=1)
-async def alarm_message(message):
-	await client.wait_until_ready()
-	channel = client.get_channel(channel_id)
-	
-	if alarm_message.author == client.user:
-			return
-	if alarm_message.content.startswith('!schedule'):
-			await alarm_message.channel.send('You got it!')
-    # message = 'test'
-    # await channel.send(message)
-alarm_message.start()
+
+@tasks.loop(seconds=5)
+async def alarm_message():
+
+    await client.wait_until_ready()
+    channel = client.get_channel(channel_id)
+
+    await channel.send('I am going to send a message every 5 seconds!')
+
+
+@client.event
+async def on_message(message):
+
+	if message.author == client.user:
+		return
+	if message.content.startswith('!schedule'):
+		alarm_message.start()
 
 client.run(TOKEN)
 
@@ -39,8 +45,8 @@ client.run(TOKEN)
 # 	if message.content.startswith('!schedule'):
 # 		await message.channel.send('You got it!')
 
-        # while True:
-        # 	time = datetime.datetime.today()
+# while True:
+# 	time = datetime.datetime.today()
 
-        # 	if message.content.startswith('!schedule {time.hour}:{time.minute}'):
-        # 		await client.get_channel(channel_id).send(f"It is {time.hour}:{time.minute}!")
+# 	if message.content.startswith('!schedule {time.hour}:{time.minute}'):
+# 		await client.get_channel(channel_id).send(f"It is {time.hour}:{time.minute}!")
